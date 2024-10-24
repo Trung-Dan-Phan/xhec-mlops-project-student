9ab85ed385b6c93292990bac40ab8863327c113a
Yuding Duan <danbzbr@gmail.com>
2024-10-24 20:22:42 +0200

    update branch 4

--- src/modelling/deployment.py
+++ src/modelling/deployment.py
@@ -4,26 +4,45 @@ from config import DATA_DIRPATH, MODELS_DIRPATH
 from prefect import serve
 from workflows import batch_predict_workflow, train_model_workflow
 
+df_path = "https://raw.githubusercontent.com/Trung-Dan-Phan/xhec-mlops-project-student/refs/heads/3/use_prefect/data/abalone.csv"
+
+
 if __name__ == "__main__":
+    # Load the training data
+    #training_data = pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).head(3000)
+    # training_data = pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).head(3000)
+    training_data = pd.read_csv(df_path).head(3000)
+
+    # Define the model training deployment
     train_model_deployment = train_model_workflow.to_deployment(
         name="Model training Deployment",
         version="0.1.0",
         tags=["training", "model"],
-        cron="0 0 * * 0",
+        cron="0 0 * * 0",  # Run every Sunday at midnight
         parameters={
-            "df": pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).head(3000),
+            #"df": pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).head(3000).to_json(),
+            'df_path': df_path,
             "artifacts_filepath": MODELS_DIRPATH,
         },
     )
 
+    # Load the batch prediction data
+    # batch_prediction_data = pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).tail(1000)
+    batch_prediction_data = pd.read_csv(df_path).tail(1000)
+
+    # Define the batch prediction deployment
     batch_predict_deployment = batch_predict_workflow.to_deployment(
         name="Batch predict Deployment",
         version="0.1.0",
         tags=["inference"],
-        interval=600,
+        interval=600,  # Run every 10 minutes
         parameters={
-            "df": pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).tail(1000),
+            #"df": pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).tail(1000).to_json(),
+            "batch_df_path": df_path,
             "artifacts_filepath": MODELS_DIRPATH,
         },
     )
+
+    # Serve both deployments
     serve(train_model_deployment, batch_predict_deployment)
+
