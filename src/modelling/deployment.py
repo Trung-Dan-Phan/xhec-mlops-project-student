056e7dac3161a26af13160bd21c4ebe8b9f61e79
Yuding Duan <danbzbr@gmail.com>
2024-10-24 17:53:50 +0200

    refacto deployment workflows

--- src/modelling/deployment.py
+++ src/modelling/deployment.py
@@ -4,6 +4,9 @@ from config import DATA_DIRPATH, MODELS_DIRPATH
 from prefect import serve
 from workflows import batch_predict_workflow, train_model_workflow
 
+df_path = "https://raw.githubusercontent.com/Trung-Dan-Phan/xhec-mlops-project-student/refs/heads/3/use_prefect/data/abalone.csv"
+
+
 if __name__ == "__main__":
     # Load the training data
     training_data = pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).head(3000)
@@ -15,7 +18,8 @@ if __name__ == "__main__":
         tags=["training", "model"],
         cron="0 0 * * 0",  # Run every Sunday at midnight
         parameters={
-            "df": training_data,
+            #"df": pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).head(3000).to_json(),
+            'df_path': df_path,
             "artifacts_filepath": MODELS_DIRPATH,
         },
     )
@@ -30,7 +34,8 @@ if __name__ == "__main__":
         tags=["inference"],
         interval=600,  # Run every 10 minutes
         parameters={
-            "batch_df": batch_prediction_data,
+            #"df": pd.read_csv(os.path.join(DATA_DIRPATH, "abalone.csv")).tail(1000).to_json(),
+            "batch_df_path": df_path,
             "artifacts_filepath": MODELS_DIRPATH,
         },
     )
