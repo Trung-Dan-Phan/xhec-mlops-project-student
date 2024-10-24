bf6aba9abe4c6cd38f6f32d85bfdf819d5666431
Trung Dan Phan <trung-dan.phan@hec.edu>
2024-10-24 16:51:02 +0200

    :recycle: Update config

--- src/modelling/config.py
+++ src/modelling/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-# [2] means go up 2 levels from the current file -> from ./lessons/03/solution/config.py to ./
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
+# [1] means go up 1 level from the current file
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATA_DIRPATH = str(PROJECT_ROOT / "data")
 MODELS_DIRPATH = str(PROJECT_ROOT / "src/web_service/local_objects")
